69527b5f39393cd77557f0a0eafdf8e84f5c7c59
Arttu Lehtonen <37210944+Anse11@users.noreply.github.com>
2018-04-12 23:11:10 +0300

    Revert "Anti_wovel"
    This reverts commit 018a9856608053c45b280802e10f5cf1ca2add45.

--- Product_multiplication.py
+++
@@ -1,6 +0,0 @@
-#Makes a list of integers as input and returns the product of all of the elements in the list. For example: product([4, 5, 5]) should return 100
-def product(list):
-  total = 1
-  for num in list:
-    total = total * num
-  return total
--- Purify_odd_numbers.py
+++
@@ -1,9 +0,0 @@
-#Purify list and return only even numbers on a new list
-
-def purify(lst):
-  a = []
-  for n in lst:
-    if n % 2 == 0:
-      result = a.append(n)
-
-      return result
